2a703186633066130fcd5321e38fc974b928d8fa
malaga28 <sebastianpiasecki2710@gmail.com>
2026-04-03 11:10:21 +0200

    Create layers.js

---
+++ config/layers.js
@@ -0,0 +1,16 @@
+const layersConfig = {
+  historical: {
+    "hist1": "data/olive_suitability/hist_1.tif",
+    "hist2": "data/olive_suitability/hist_2.tif"
+  },
+  future: {
+    ssp126: {
+      "future1": "data/olive_suitability/ssp126_future_1.tif",
+      "future2": "data/olive_suitability/ssp126_future_2.tif"
+    },
+    ssp585: {
+      "future1": "data/olive_suitability/ssp585_future_1.tif",
+      "future2": "data/olive_suitability/ssp585_future_2.tif"
+    }
+  }
+};
