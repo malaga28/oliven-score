d69a4c81c41745a14e8af1b90b3f486569c43487
malaga28 <sebastianpiasecki2710@gmail.com>
2026-04-03 11:19:03 +0200

    Update layers.js

--- config/layers.js
+++ config/layers.js
@@ -1,16 +1,18 @@
 const layersConfig = {
   historical: {
-    "hist1": "data/olive_suitability/hist_1.tif",
-    "hist2": "data/olive_suitability/hist_2.tif"
+    "1951-1980": "MPI_Weighted_FrostKill_Score_HISTORICAL_1951_1980.tif",
+    "1981-2014": "MPI_Weighted_FrostKill_Score_HISTORICAL_1981_2014.tif"
   },
   future: {
-    ssp126: {
-      "future1": "data/olive_suitability/ssp126_future_1.tif",
-      "future2": "data/olive_suitability/ssp126_future_2.tif"
+    ssp245: {
+      "2015-2040": "MPI_Weighted_FrostKill_Score_SSP245_2015_2040.tif",
+      "2041-2070": "MPI_Weighted_FrostKill_Score_SSP245_2041_2070.tif",
+      "2071-2100": "MPI_Weighted_FrostKill_Score_SSP245_2071_2100.tif"
     },
     ssp585: {
-      "future1": "data/olive_suitability/ssp585_future_1.tif",
-      "future2": "data/olive_suitability/ssp585_future_2.tif"
+      "2015-2040": "MPI_Weighted_FrostKill_Score_SSP585_2015_2040.tif",
+      "2041-2070": "MPI_Weighted_FrostKill_Score_SSP585_2041_2070.tif",
+      "2071-2100": "MPI_Weighted_FrostKill_Score_SSP585_2071_2100.tif"
     }
   }
 };
