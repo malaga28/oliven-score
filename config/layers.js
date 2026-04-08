8160a058fb5340ab01cf99934383a47abe780898
malaga28 <sebastianpiasecki2710@gmail.com>
2026-04-08 12:04:20 +0200

    Update layers.js

--- config/layers.js
+++ config/layers.js
@@ -1,18 +1,41 @@
 const layersConfig = {
-  historical: {
-    "1951-1980": "MPI_Weighted_FrostKill_Score_HISTORICAL_1951_1980.tif",
-    "1981-2014": "MPI_Weighted_FrostKill_Score_HISTORICAL_1981_2014.tif"
-  },
-  future: {
-    ssp245: {
-      "2015-2040": "MPI_Weighted_FrostKill_Score_SSP245_2015_2040.tif",
-      "2041-2070": "MPI_Weighted_FrostKill_Score_SSP245_2041_2070.tif",
-      "2071-2100": "MPI_Weighted_FrostKill_Score_SSP245_2071_2100.tif"
+  scoreRasters: {
+    historical: {
+      "1951-1980": "MPI_Weighted_FrostKill_Score_HISTORICAL_1951_1980.tif",
+      "1981-2014": "MPI_Weighted_FrostKill_Score_HISTORICAL_1981_2014.tif"
     },
-    ssp585: {
-      "2015-2040": "MPI_Weighted_FrostKill_Score_SSP585_2015_2040.tif",
-      "2041-2070": "MPI_Weighted_FrostKill_Score_SSP585_2041_2070.tif",
-      "2071-2100": "MPI_Weighted_FrostKill_Score_SSP585_2071_2100.tif"
+    future: {
+      ssp245: {
+        "2015-2040": "MPI_Weighted_FrostKill_Score_SSP245_2015_2040.tif",
+        "2041-2070": "MPI_Weighted_FrostKill_Score_SSP245_2041_2070.tif",
+        "2071-2100": "MPI_Weighted_FrostKill_Score_SSP245_2071_2100.tif"
+      },
+      ssp585: {
+        "2015-2040": "MPI_Weighted_FrostKill_Score_SSP585_2015_2040.tif",
+        "2041-2070": "MPI_Weighted_FrostKill_Score_SSP585_2041_2070.tif",
+        "2071-2100": "MPI_Weighted_FrostKill_Score_SSP585_2071_2100.tif"
+      }
+    }
+  },
+
+  oliveRaster: "olives_4326.tif",
+
+  chartCSVs: {
+    historical: {
+      "1951-1980": "1951.csv",
+      "1981-2014": "1981.csv"
+    },
+    future: {
+      ssp245: {
+        "2015-2040": "2015_ssp245.csv",
+        "2041-2070": "2041_ssp245.csv",
+        "2071-2100": "2071_ssp245.csv"
+      },
+      ssp585: {
+        "2015-2040": "2015_ssp585.csv",
+        "2041-2070": "2041_ssp585.csv",
+        "2071-2100": "2071_ssp585.csv"
+      }
     }
   }
 };
