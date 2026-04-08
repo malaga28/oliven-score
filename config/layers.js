b3f5e6524e199862d40643006b61066c5e6ad242
malaga28 <sebastianpiasecki2710@gmail.com>
2026-04-08 12:10:11 +0200

    Update layers.js

--- config/layers.js
+++ config/layers.js
@@ -1,19 +1,19 @@
 const layersConfig = {
   scoreRasters: {
     historical: {
-      "1951-1980": "MPI_Weighted_FrostKill_Score_HISTORICAL_1951_1980.tif",
-      "1981-2014": "MPI_Weighted_FrostKill_Score_HISTORICAL_1981_2014.tif"
+      "1951-1980": "MPI_Weighted_Climate_Suitability_Score_HISTORICAL_1951_1980.tif",
+      "1981-2014": "MPI_Weighted_Climate_Suitability_Score_HISTORICAL_1981_2014.tif"
     },
     future: {
       ssp245: {
-        "2015-2040": "MPI_Weighted_FrostKill_Score_SSP245_2015_2040.tif",
-        "2041-2070": "MPI_Weighted_FrostKill_Score_SSP245_2041_2070.tif",
-        "2071-2100": "MPI_Weighted_FrostKill_Score_SSP245_2071_2100.tif"
+        "2015-2040": "MPI_Weighted_Climate_Suitability_Score_SSP245_2015_2040.tif",
+        "2041-2070": "MPI_Weighted_Climate_Suitability_Score_SSP245_2041_2070.tif",
+        "2071-2100": "MPI_Weighted_Climate_Suitability_Score_SSP245_2071_2100.tif"
       },
       ssp585: {
-        "2015-2040": "MPI_Weighted_FrostKill_Score_SSP585_2015_2040.tif",
-        "2041-2070": "MPI_Weighted_FrostKill_Score_SSP585_2041_2070.tif",
-        "2071-2100": "MPI_Weighted_FrostKill_Score_SSP585_2071_2100.tif"
+        "2015-2040": "MPI_Weighted_Climate_Suitability_Score_SSP585_2015_2040.tif",
+        "2041-2070": "MPI_Weighted_Climate_Suitability_Score_SSP585_2041_2070.tif",
+        "2071-2100": "MPI_Weighted_Climate_Suitability_Score_SSP585_2071_2100.tif"
       }
     }
   },
